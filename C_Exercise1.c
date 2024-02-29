36e3a6d73febe7eaa8f0e515bcce27ec5be25bcd
Nuralim Otegen <nuralimutegen@gmail.com>
2024-03-01 00:50:03 +0500

    Nura_v2

--- C_Exercise1.c
+++ C_Exercise1.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     int distance = 100;
-    printf("You are %i miles away.\n", distance);
+    printf("You are %i miles away.\n", distance);  //By using %f/i/d we can put the variable to the string
     return 0;
 }
 
